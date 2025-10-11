6e0cb6f6a57babe279f6a16b23986852650020eb
Abigail Read <AbbyReadMail@gmail.com>
2025-10-10 19:48:40 -0700

    Get the XP bar to dim properly

--- src/main/java/net/fabricmc/abbyread/mixin/GuiIngameMixin.java
+++ src/main/java/net/fabricmc/abbyread/mixin/GuiIngameMixin.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -51,4 +52,44 @@ public abstract class GuiIngameMixin {
     private void postHudRender(float partialTicks, boolean hasScreen, int mouseX, int mouseY, CallbackInfo ci) {
         GL11.glColor4f(1f, 1f, 1f, 1f);
     }
+
+    @Unique
+    private boolean pushedXpAttrib = false;
+
+    @Inject(
+            method = "renderGameOverlay",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/src/PlayerControllerMP;func_78763_f()Z",
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void preXpBlock(float partialTicks, boolean hasScreen, int mouseX, int mouseY, CallbackInfo ci) {
+        if (mc == null || mc.thePlayer == null) return;
+
+        float brightness = BrightnessHelper.getCurrentHUDLight(mc.thePlayer);
+        GL11.glPushAttrib(GL11.GL_CURRENT_BIT);
+        pushedXpAttrib = true;
+        GL11.glColor4f(brightness, brightness, brightness, 1.0F);
+    }
+
+    @Inject(
+            method = "renderGameOverlay",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/src/Profiler;endSection()V",
+                    shift = At.Shift.BEFORE
+            )
+    )
+    private void postXpBlock(float partialTicks, boolean hasScreen, int mouseX, int mouseY, CallbackInfo ci) {
+        if (!pushedXpAttrib) return; // only pop if we actually pushed
+        pushedXpAttrib = false;
+
+        try {
+            GL11.glPopAttrib();
+        } catch (Throwable t) {
+            GL11.glColor4f(1f, 1f, 1f, 1f);
+        }
+    }
+
 }
